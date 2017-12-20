fcbf3a1f1414aba0254f39092a4c8ad97ba6600f
Vidor Gencel <vidorge@gmail.com>
2017-12-20 14:19:19 +0100

    Bootstraped crowdsale deploy

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,62 @@
+var BestMillionsCrowdsale = artifacts.require('BestMillionsCrowdsale.sol');
+
+module.exports = function(deployer, network, accounts) {
+  // Use deployer to state migration tasks.
+
+  return liveDeploy(deployer, accounts);
+
+};
+
+function latestTime () {
+
+  return web3.eth.getBlock('latest').timestamp;
+
+}
+
+function ether (n) {
+
+  return new web3.BigNumber(web3.toWei(n, 'ether'));
+
+}
+
+const duration = {
+  seconds: function (val) { return val; },
+  minutes: function (val) { return val * this.seconds(60); },
+  hours: function (val) { return val * this.minutes(60); },
+  days: function (val) { return val * this.hours(24); },
+  weeks: function (val) { return val * this.days(7); },
+  years: function (val) { return val * this.days(365); },
+};
+
+async function liveDeploy(deployer, accounts) {
+
+  const BigNumber = web3.BigNumber;
+
+  const startTime = latestTime() + duration.weeks(1);
+  const endTime   = startTime + duration.weeks(1);
+  const RATE      = new BigNumber(16000);
+  const cap       = ether(12187,5);
+  const goal      = ether(2437,5);  
+  const coinCap   = new BigNumber(300000000);
+
+  console.log([
+    startTime,
+    endTime,
+    RATE.toNumber(),
+    accounts[0],
+    cap.toNumber(),
+    goal.toNumber(),
+    coinCap.toNumber()
+  ]);
+
+  // uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, uint256 _cap, uint256 _goal, uint256 _coinCap
+  return deployer.deploy(BestMillionsCrowdsale, startTime, endTime, RATE, accounts[0], cap, goal, coinCap).then(async () => {
+
+    const instance  = await BestMillionsCrowdsale.deployed();
+    const token     = await instance.token.call();
+
+    console.log('Token address', token);
+
+  });
+
+}
