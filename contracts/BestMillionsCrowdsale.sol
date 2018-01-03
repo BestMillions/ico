cecc16d00be8cbea8d4f2103e8a1f214634cc3ff
Vidor Gencel <vidorge@gmail.com>
2018-01-03 23:02:39 +0100

    Initialize token time locks and minting residual supply of tokens

--- contracts/BestMillionsCrowdsale.sol
+++ contracts/BestMillionsCrowdsale.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.4.18;
 
 import '../node_modules/zeppelin-solidity/contracts/crowdsale/CappedCrowdsale.sol';
 import '../node_modules/zeppelin-solidity/contracts/crowdsale/RefundableCrowdsale.sol';
+import '../node_modules/zeppelin-solidity/contracts/token/TokenTimelock.sol';
 
 import './BestMillionsToken.sol';
 
 contract BestMillionsCrowdsale is CappedCrowdsale, RefundableCrowdsale {
 
     uint256 coinCap;
+
+    TokenTimelock[4] public timeLocks;
+
+    address[5] public addresses;
 
     function BestMillionsCrowdsale(
         uint256 _startTime,
@@ -28,6 +33,19 @@ contract BestMillionsCrowdsale is CappedCrowdsale, RefundableCrowdsale {
         require(_goal <= cap);
 
         token = new BestMillionsToken(_coinCap);
+
+        addresses[0] = 0x01;
+        addresses[1] = 0x02;
+        addresses[2] = 0x03;
+        addresses[3] = 0x04;
+        addresses[4] = 0x05;
+
+        uint64 releaseTime = uint64(_endTime + 1 years);
+
+        timeLocks[0] = new TokenTimelock(token, addresses[0], releaseTime);
+        timeLocks[1] = new TokenTimelock(token, addresses[1], releaseTime);
+        timeLocks[2] = new TokenTimelock(token, addresses[2], releaseTime);
+        timeLocks[3] = new TokenTimelock(token, addresses[3], releaseTime);
 
         PausableToken(token).pause();
 
@@ -78,9 +96,32 @@ contract BestMillionsCrowdsale is CappedCrowdsale, RefundableCrowdsale {
 
     function finalization() internal {
 
-        PausableToken(token).unpause();
+        mintResidualTokens();
 
         super.finalization();
+
+        PausableToken(token).unpause();
+
+    }
+
+    function mintResidualTokens() internal {
+
+        uint256 totalSupply = token.totalSupply();
+
+        // Team member 1 gets 7.5% of total supply locked for 1 year
+        token.mint(timeLocks[0], totalSupply.mul(75).div(740));
+        
+        // Team member 2 gets 3.5% of total supply locked for 1 year
+        token.mint(timeLocks[1], totalSupply.mul(35).div(740));
+
+        // Team member 2 gets 3.5% of total supply locked for 1 year
+        token.mint(timeLocks[2], totalSupply.mul(35).div(740));
+
+        // Team members 3 and 4 get 1% of total supply locked for 1 year
+        token.mint(timeLocks[3], totalSupply.mul(10).div(740));
+
+        // Rest of the tokens is transfered to a multisig wallet
+        token.mint(addresses[4], totalSupply.mul(100).div(740));
 
     }
 
