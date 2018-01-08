2c8344ce3755e4a02776c7751d81ac13bf65f085
Vidor Gencel <vidorge@gmail.com>
2018-01-08 15:08:53 +0100

    Token buyback

--- contracts/BestMillionsToken.sol
+++ contracts/BestMillionsToken.sol
@@ -11,12 +11,46 @@ contract BestMillionsToken is CappedToken, PausableToken, BurnableToken {
     string public constant symbol = "BMT";
     uint8 public constant decimals = 18;
 
+    uint256 public rate;
+
     function BestMillionsToken(uint256 _cap)
         CappedToken(_cap)
         public
     {
-        
+
         paused = true;
+        
+    }
+
+    function sellBack(uint256 _amount) public whenNotPaused {
+
+        require(_amount > 0);
+        require(_amount <= balances[msg.sender]);
+
+        uint256 ethAmount = _amount.div(rate);
+
+        require(ethAmount > 0);
+        require(this.balance >= ethAmount);
+
+        burn(_amount);
+
+        msg.sender.transfer(ethAmount);
+
+    }
+
+    function depositFunds() public payable { }
+
+    function withdrawFunds(uint256 _amount) onlyOwner public {
+
+        require(_amount <= this.balance);
+
+        msg.sender.transfer(_amount);
+
+    }
+
+    function setRate(uint256 _rate) onlyOwner public {
+
+        rate = _rate;
 
     }
 
